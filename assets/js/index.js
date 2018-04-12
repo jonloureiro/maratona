05cf1141581b41d115bd085873f4c63d7f3a57c3
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2018-04-12 17:27:06 -0400

    testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,10 @@
 console.log(document.getElementById('card-home'));
 
 if (document.getElementById('card-home') == null) {
-  console.log(document.getElementById('sidenav'));
-  document.getElementById('sidenav').classList.add('fadeInRight');
+  let sidenav = document.getElementById('sidenav');
+  if (sidenav.classList.contains('fadeInRight')) {
+    console.log(sidenav);
+  } else {
+    sidenav.classList.add('fadeInRight');
+  }
 }
